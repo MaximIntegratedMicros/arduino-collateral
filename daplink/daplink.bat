3e1712cab6d8bd152106bb1d21cb48b10cc13307
Keyur Hariya <Keyur.Hariya@maximintegrated.com>
2019-02-13 13:34:22 -0600

    Avoid searching for Maxim board by exactly specifying the mount point
    Significantly improves upload speed on unix-like machines with multiple/large volumes.
    Match the code flow on Windows as closely as possible.

--- daplink/daplink.bat
+++ daplink/daplink.bat
@@ -2,37 +2,31 @@
 
 set binPath=%1
 set binFile=%2
-set options=%3
+set driveName=%3
+set options=%4
+
 set drivePath=
 
-for /f "skip=2" %%x in (
-    'wmic logicaldisk get deviceid'
-    ) do (
-        if "%options%" == "-v" (
-            echo.Searching for Maxim board at %%x
-        )
-        if exist %%x\DETAILS.TXT (
-            if exist %%x\MBED.HTM (
-                set drivePath=%%x
-                goto found
-            ) else (
-                if exist %%x\HELP_FAQ.HTM (
-                    set drivePath=%%x
-                    goto found
-                )
-            )
-        )
-    )
+for /f %%x in (
+    'wmic logicaldisk where "VolumeName='%driveName%'" get DeviceID ^| find ":"'
+    ) do set drivePath=%%x
+if "%options%" == "-v" (
+    echo.Searching for Maxim board at %drivePath%
+)
+if exist %drivePath%\DETAILS.TXT (
+    if "%driveName%" == "BOOTLOADER" if exist %drivePath%\HELP_FAQ.HTM goto found
+    if "%driveName%" == "DAPLINK" if exist %drivePath%\MBED.HTM goto found
+)
 echo.Maxim board not found.
 ::Exit code 101 to allow Arduino IDE in raising error condition
 exit /B 101
 
 :found
-    copy /Y /b %binPath%\%binFile% %drivePath%\ >nul
+    copy /Y /B %binPath%\%binFile% %drivePath%\ >nul
     if "%options%" == "-v" (
         echo.%binPath%\%binFile% -^> %drivePath%\%binFile%
     )
     if not exist %drivePath%\%binFile% (
         echo.Can't upload the file to USB drive.
         exit /B 102
-        )
+    )
